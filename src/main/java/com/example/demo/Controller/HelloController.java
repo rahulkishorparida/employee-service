66393a35d7cefde223807e7490b98d0abb07fefd
rahulkishorparida <rahulkishor943@gmail.com>
2026-09-18 17:38:34 +0530

    end point update

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/employee")
 public class HelloController {
 
-    @GetMapping("/helloo")
+    @GetMapping("/hello")
     public String hello() {
         return " Welcome to your Dashboard!!";
     }
